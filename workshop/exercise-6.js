57994fd7d632982284056fa44657cf53b971e56b
myriam mansouri <myriam.mansouri@gmail.com>
2020-06-16 16:18:03 -0400

    Exercise 6

--- workshop/exercise-6.js
+++ workshop/exercise-6.js
@@ -6,8 +6,11 @@
 // Q6
 // Write a function that accepts a list of numbers and returns a new list with
 // all of the even numbers removed.
-function removeEvens(lst) {
-  // lst is an array of strings
+const removeEvens = function(lst) {
+  return lst.filter(el => el % 2 ===0)
 }
+
+  // lst is an array of strings
+
 // -------------------------------------------------------------------------
 console.log('Q4 removeEvens()', removeEvens([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
