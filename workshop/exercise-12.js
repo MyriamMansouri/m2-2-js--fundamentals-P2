c9049ef72cf9f4c2979af434a52c498da77b2cea
myriam mansouri <myriam.mansouri@gmail.com>
2020-06-16 16:50:50 -0400

    Exercise 12

--- workshop/exercise-12.js
+++ workshop/exercise-12.js
@@ -13,3 +13,26 @@
 // Call the function with [48, 95, 65, 48, 59, 78, 72, 65]
 
 // Try with other values as well
+
+const calculateLetterGrade = function(grades) {
+    average = Math.round(grades.reduce( (acc, value) => acc + value) / grades.length);
+
+    let grade = "";
+
+    if (average < 60 ) {
+        grade = "F"
+    } else if (average < 70 ) {
+        grade = "D"
+    } else if (average < 80 ) {
+        grade = "C"
+    } else if (average < 90 ) {
+        grade = "B"
+    } else {
+        grade = "A"
+    };
+
+    return grade;
+}
+
+console.log(calculateLetterGrade([48, 95, 65, 48, 59, 78, 72, 65]));
+console.log(calculateLetterGrade([98, 90]));
